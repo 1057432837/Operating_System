a4d82cc91699ae2148f6e4922ba1863590ac37ae
1057432837 <lxc.990131@gmail.com>
2024-07-05 11:52:03 +0800

    2024.7.5 11:52

--- kernel/main.c
+++ kernel/main.c
@@ -13,29 +13,37 @@ void init(void);
 
 int main(void)
 {
-    put_str("I am kernel\n");
-    init_all();
+    // put_str("I am kernel\n");
+    // init_all();
 
-    uint32_t file_size = 21816;
-    uint32_t sec_cnt = DIV_ROUND_UP(file_size, 512);
-    struct disk *sda = &channels[0].devices[0];
-    void *prog_buf = sys_malloc(file_size);
-    ide_read(sda, 300, prog_buf, sec_cnt);
-    int32_t fd = sys_open("/cat", O_CREAT | O_RDWR);
-    if (fd != -1)
+    // uint32_t file_size = 21816;
+    // uint32_t sec_cnt = DIV_ROUND_UP(file_size, 512);
+    // struct disk *sda = &channels[0].devices[0];
+    // void *prog_buf = sys_malloc(file_size);
+    // ide_read(sda, 300, prog_buf, sec_cnt);
+    // int32_t fd = sys_open("/cat", O_CREAT | O_RDWR);
+    // if (fd != -1)
+    // {
+    //     if (sys_write(fd, prog_buf, file_size) == -1)
+    //     {
+    //         printk("file write error!\n");
+    //         while (1)
+    //             ;
+    //     }
+    // }
+
+    // cls_screen();
+    // console_put_str("[rabbit@localhost /]$ ");
+    // thread_exit(running_thread(), true);
+    // return 0;
+    int i;
+    char *p;
+    for (i = 0xa0000; i < 0xaffff; i++)
     {
-        if (sys_write(fd, prog_buf, file_size) == -1)
-        {
-            printk("file write error!\n");
-            while (1)
-                ;
-        }
+        p = i;
+        *p = i & 0x0f;
     }
-
-    cls_screen();
-    console_put_str("[rabbit@localhost /]$ ");
-    thread_exit(running_thread(), true);
-    return 0;
+    while (1);
 }
 
 /* init进程 */
