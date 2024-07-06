7f2c71558f4f54df2854d024f18f857f17d59df9
1057432837 <lxc.990131@gmail.com>
2024-07-06 09:11:16 +0800

    2024.7.6 09:11

--- kernel/main.c
+++ kernel/main.c
@@ -9,6 +9,23 @@
 #include "ide.h"
 #include "stdio-kernel.h"
 
+#define COL8_000000   0
+#define COL8_FF0000   1
+#define COL8_00FF00   2
+#define COL8_FFFF00   3
+#define COL8_0000FF   4
+#define COL8_FF00FF   5
+#define COL8_00FFFF   6
+#define COL8_FFFFFF   7
+#define COL8_c6c6c6   8
+#define COL8_840000   9
+#define COL8_008400   10
+#define COL8_848400   11
+#define COL8_000084   12
+#define COL8_840084   13
+#define COL8_008484   14
+#define COL8_848484   15
+
 extern void io_cli(void);
 extern void io_out8(int port, int data);
 extern int io_load_eflags(void);
@@ -17,6 +34,7 @@ extern void io_store_eflags(int eflags);
 void init(void);
 void init_palette(void);
 void set_palette(int start, int end, unsigned char* rgb);
+void boxfill8(unsigned char *vram, int xsize, unsigned char c, int x0, int y0, int x1, int y1);
 
 int main(void)
 {
@@ -43,14 +61,14 @@ int main(void)
     // console_put_str("[rabbit@localhost /]$ ");
     // thread_exit(running_thread(), true);
     // return 0;
-    int i;
     char *p;
     init_palette();
     p = (char*) 0xa0000;
-    for (i = 0; i <= 0xffff; i++)
-    {
-        p[i] = i & 0x0f;
-    }
+
+    boxfill8(p, 320, COL8_FF0000, 20, 20, 120, 120);
+    boxfill8(p, 320, COL8_00FF00, 70, 50, 170, 150);
+    boxfill8(p, 320, COL8_0000FF, 120, 80, 220, 180);
+
     while (1);
 }
 
@@ -90,6 +108,18 @@ void set_palette(int start, int end, unsigned char* rgb) {
         rgb += 3;
     }
     io_store_eflags(eflags);
+    return;
+}
+
+void boxfill8(unsigned char *vram, int xsize, unsigned char c, int x0, int y0, int x1, int y1) {
+    int x, y;
+    for (y = y0; y <= y1; y++)
+    {
+        for (x = x0; x <= x1; x++)
+        {
+           vram[y * xsize + x] = c;
+        }
+    }
     return;
 }
 
